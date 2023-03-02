d83fb08e11762ce503d33064267fee0160c06160
sajosu <117356563+sajosu@users.noreply.github.com>
2023-03-02 15:34:22 +0200

    Update headers.php

--- inc/headers.php
+++ inc/headers.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
-header('Access-Control-Max-Age: 84600');
+header('Access-Control-Max-Age: 86400');
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
